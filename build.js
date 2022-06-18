b6ab0252647cb93e40e19cbbdf66a0a47b354f71
davidjp1 <35931909+davidjp1@users.noreply.github.com>
2022-06-18 12:54:49 +0100

    esbuild basic setup

--- build.js
+++ build.js
@@ -1,5 +1,8 @@
+/* eslint-disable */
 const fs = require('fs');
 const path = require('path');
+
+require('dotenv').config();
 
 const copyRecursiveSync = function(src, dest) {
   const exists = fs.existsSync(src);
@@ -15,15 +18,34 @@ const copyRecursiveSync = function(src, dest) {
   }
 };
 
-copyRecursiveSync('./public', './build');
+const build = async (hotswap) => {
+  const define = { "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development") };
 
-require('esbuild').build({
-  bundle: true,
-  define: { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') },
-  entryPoints: ['./src/index.tsx'],
-  sourcemap: true,
-  minify: process.env.NODE_ENV === 'production',
-  outfile: 'build/bundle.js',
-  loader: {'.js': 'jsx'},
-  target: ["chrome58", "edge18", "safari11", "firefox57"]
-}).catch(() => process.exit(1));
+  for (const k in process.env) {
+    console.log(k);
+    define[`process.env.${k}`] = JSON.stringify(process.env[k])
+  }
+  
+  
+
+  if(fs.existsSync('./build')) fs.rmSync('./build', {recursive: true, force: true});
+  fs.mkdirSync('./build');
+  copyRecursiveSync('./public', './build');
+
+  return require('esbuild').build({
+    incremental: hotswap,
+    inject: ['./react-inject.js'],
+    bundle: true,
+    define,
+    entryPoints: ['./src/index.tsx'],
+    sourcemap: true,
+    minify: process.env.NODE_ENV === 'production',
+    outfile: './build/static/bundle.js',
+    loader: {'.js': 'jsx'},
+    target: ["chrome58", "edge18", "safari11", "firefox57"]
+  }).catch(() => process.exit(1));
+};
+
+build(false);
+
+module.exports = {build};
--- serve.js
+++ serve.js
@@ -1,6 +1,6 @@
 // source: https://github.com/zaydek/esbuild-hot-reload/blob/master/serve.js
-// eslint-disable-line
-const { build } = require('esbuild');
+/* eslint-disable */
+const { build } = require('./build');
 // eslint-disable-line
 const chokidar = require('chokidar');
 // eslint-disable-line
@@ -8,36 +8,27 @@ const liveServer = require('live-server');
 const fs = require('fs');
 const path = require('path');
 
-// const copyRecursiveSync = function(src, dest) {
-//   const exists = fs.existsSync(src);
-//   const stats = exists && fs.statSync(src);
-//   const isDirectory = exists && stats.isDirectory();
-//   if (isDirectory) {
-//     fs.readdirSync(src).forEach(function(childItemName) {
-//       copyRecursiveSync(path.join(src, childItemName),
-//                         path.join(dest, childItemName));
-//     });
-//   } else {
-//     fs.copyFileSync(src, dest);
-//   }
-// };
+const copyRecursiveSync = function(src, dest) {
+  const exists = fs.existsSync(src);
+  const stats = exists && fs.statSync(src);
+  const isDirectory = exists && stats.isDirectory();
+  if (isDirectory) {
+    fs.readdirSync(src).forEach(function(childItemName) {
+      copyRecursiveSync(path.join(src, childItemName),
+                        path.join(dest, childItemName));
+    });
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+};
 
 (async () => {
-  // copyRecursiveSync('./public', './build');
+  copyRecursiveSync('./public', './build');
   fs.copyFileSync
-  const builder = await build({
-    bundle: true,
-    define: { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') },
-    entryPoints: ['./src/index.tsx'],
-    sourcemap: true,
-    minify: process.env.NODE_ENV === 'production',
-    outfile: 'build/bundle.js',
-    incremental: true,
-    target: ["chrome58", "edge18", "safari11", "firefox57"]
-  });
+  const builder = await build(true);
   chokidar
     .watch('./src/**/*.{js,jsx,ts,tsx,html}', {
-      interval: 10,
+      interval: 0,
     })
     .on('all', () => {
       builder.rebuild();
