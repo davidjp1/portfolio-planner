670affa397d775a5fdaa758ba92dbe37ae514f2e
davidjp1 <35931909+davidjp1@users.noreply.github.com>
2022-06-20 23:31:30 +0100

    fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase/firestore';
+import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import express from 'express';
 import cors from 'cors';
